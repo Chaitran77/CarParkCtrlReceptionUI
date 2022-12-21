7528830bf04bccadb4f3b6fdc8f12c6d4e62c6f2
Kiran Patel <kiran@inteleyes.com>
2022-12-21 21:31:01 +0000

    Changed alignment of "Open Gate" button text to CENTER Changed indentation to tabs

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.securitysystems.carparkctrlreceptionui {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
+    requires com.goxr3plus.fxborderlessscene;
 
 //    requires org.controlsfx.controls;
 //    requires org.kordamp.bootstrapfx.core;
