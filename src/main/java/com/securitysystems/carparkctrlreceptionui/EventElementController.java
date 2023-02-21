1ae3bd6352222e01def984498b0c3aef64ef4ec5
Kiran Patel <kiran@inteleyes.com>
2023-02-21 23:21:27 +0000

    Cleaned up CSS

--- src/main/java/com/securitysystems/carparkctrlreceptionui/EventElementController.java
+++ src/main/java/com/securitysystems/carparkctrlreceptionui/EventElementController.java
@@ -1,4 +1,19 @@
 package com.securitysystems.carparkctrlreceptionui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+
 public class EventElementController {
+
+	@FXML
+	protected void handleClick(MouseEvent event) {
+		System.out.println("EVENT ELEM CLICKED");
+//		change colour and load event data in UI
+//		GridPane eventElement = (GridPane) event.getSource();
+//		GridPane eventElement = (GridPane) event.getPickResult().getIntersectedNode();
+//		eventElement.setStyle("-fx-background-color:yellow;");
+	}
 }
