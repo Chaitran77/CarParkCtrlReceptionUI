ecfb32c7e6303691460c2c8b4232bf03c55d5a76
Kiran Patel <kiran@inteleyes.com>
2023-02-27 23:29:22 +0000

    Started implementing search functionality

--- src/main/java/com/securitysystems/carparkctrlreceptionui/CarparkMonitoringController.java
+++ src/main/java/com/securitysystems/carparkctrlreceptionui/CarparkMonitoringController.java
@@ -1,11 +1,22 @@
 package com.securitysystems.carparkctrlreceptionui;
 
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 
 public class CarparkMonitoringController {
+
+	public Scene monitoringScene;
+	public void setMonitoringScene(Scene scene) {
+		this.monitoringScene = scene;
+	}
 
 	@FXML
 	protected void handleOpenGateButton() {
 		System.out.println("open gate button clicked");
+	}
+
+	@FXML
+	protected void handleSearchButtonClick() {
+		SearchUtils.performSearch();
 	}
 }
--- src/main/java/com/securitysystems/carparkctrlreceptionui/SearchUtils.java
+++ src/main/java/com/securitysystems/carparkctrlreceptionui/SearchUtils.java
@@ -1,8 +1,21 @@
 package com.securitysystems.carparkctrlreceptionui;
 
+import javafx.util.Pair;
+
+import java.util.Hashtable;
+
 public class SearchUtils {
 //	public Log searchEventID(int EventID) {
 //		// binary search for EventID
 //
 //	}
+
+
+	public static Hashtable<String, Object> getSearchParameters() {
+		return new Hashtable<String, Object>();
+	}
+
+	public static void performSearch() {
+		getSearchParameters();
+	}
 }
